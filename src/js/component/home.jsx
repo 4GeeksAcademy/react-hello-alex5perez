d3b759dd238d1f354c87e4fc815613c4a69e5133
Alex Perez Rubio <71727357+alex5perez@users.noreply.github.com>
2024-11-09 18:15:56 +0000

    [Se añaden las importanciones en home] ---home.jsx: ✅ Se añaden las importaciones de las clases de Navbar, jumbotron y card.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
