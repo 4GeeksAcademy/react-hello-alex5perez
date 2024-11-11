0f75830dbe5c663d800a0f2ed3d051a91504d21f
Alex Perez Rubio <71727357+alex5perez@users.noreply.github.com>
2024-11-11 18:21:32 +0000

    [Se fija el footer al final de la pagina] ---footer.jsx: ✅ Se añade fixed-bottom en la clase del footer.

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(){
     return(
-        <footer className="bg-dark py-3">
+        <footer className="bg-dark py-3 fixed-bottom">
             <p className="text-white">Footer</p>
         </footer>
     )
